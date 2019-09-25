a9bdd5c7f94751ee9898365b3fc569baf0f3bc59
XtomtomX <thomas.kolb@student.tuwien.ac.at>
2019-09-25 12:55:20 +0200

    Aufgabenstellung fertig, mehr Doku, Methodentemplates bereitgestellt.

--- JAVA - Version/src/DPQ.java
+++ JAVA - Version/src/DPQ.java
@@ -1,6 +1,7 @@
 // Java implementierung des Dijkstra Algorithmus
 // mittels einer Prioritätswarteschlange (PriorityQueue)
 // Quelle: https://www.geeksforgeeks.org/dijkstras-shortest-path-algorithm-in-java-using-priorityqueue/
+import org.jetbrains.annotations.NotNull;
 import java.util.*;
 
 public class DPQ {
@@ -8,10 +9,9 @@ public class DPQ {
     private Set<Integer> settled;
     private PriorityQueue<Node> pq;
     private int V; // Anzahl der Knoten
-    List<List<Node> > adj;
+    List<List<Node>> adj;
 
-    public DPQ(int V)
-    {
+    public DPQ(int V) {
         this.V = V;
         dist = new int[V];
         settled = new HashSet<Integer>();
@@ -19,8 +19,7 @@ public class DPQ {
     }
 
     // Funktion des Dijkstra Algorithmus
-    public void dijkstra(List<List<Node> > adj, int src)
-    {
+    public void dijkstra(List<List<Node>> adj, int src) {
         this.adj = adj;
 
         for (int i = 0; i < V; i++)
@@ -45,8 +44,7 @@ public class DPQ {
     }
 
     // Nachbarn des uebergebenen Knoten durcharbeiten
-    private void e_Neighbours(int u)
-    {
+    private void e_Neighbours(int u) {
         int edgeDistance = -1;
         int newDistance = -1;
 
@@ -69,13 +67,12 @@ public class DPQ {
         }
     }
 
-    public static void main(String arg[])
-    {
-        int V = 5;
-        int source = 0;
+    public static void main(String arg[]) {
+        int V = 0; // TODO hier aendern
+        int source = 0; // startpunkt
 
         // Adjazenzliste
-        List<List<Node> > adj = new ArrayList<List<Node> >();
+        List<List<Node>> adj = new ArrayList<List<Node>>();
 
         // Liste fuer jeden Knoten initialisieren
         for (int i = 0; i < V; i++) {
@@ -83,45 +80,8 @@ public class DPQ {
             adj.add(item);
         }
 
-        // Beispiel Input für den DPQ Graphen
-        adj.get(0).add(new Node(1, 9));
-        adj.get(0).add(new Node(2, 6));
-        adj.get(0).add(new Node(3, 5));
-        adj.get(0).add(new Node(4, 3));
-
-        adj.get(2).add(new Node(1, 2));
-        adj.get(2).add(new Node(3, 4));
-
-        /**
-         * TODO START: Aufgabe 1
-         * Adjazenzliste welche zuvor erstellt wurde einprogrammieren
-         */
-
-        /* TODO END: Aufgabe 1 */
-
-        /**
-         * TODO START: Aufgabe 2 Funktionalitaet 1
-         * Konsolenanwendung entwickeln, mit welcher es möglich ist,
-         * einen Graphen wie in Aufgabe 1 über die Kommandozeile einzutragen.
-         */
-
-        /* TODO END: Aufgabe 2 Funktionalitaet 1*/
-
-        /**
-         * TODO START: Aufgabe 2 Funktionalitaet 2
-         * Es soll möglich sein, mittels eines Kommandos den Graphen auszugeben
-         * (entweder eine Adjazenzliste oder eine grafische Darstellung)
-         */
-
-        /* TODO END Aufgabe 2 Funktionalitaet 2 */
-
-        /**
-         * TODO START: Aufgabe 2 Funkionalitaet 3
-         * Eine zusaetzliche Funktion implementieren, wie z.B. das entfernen eines zuvor eingetragenen
-         * Knoten bzw. in weiterer Folge einer Verbindung zwischen zwei Knoten.
-         */
-
-        /* TODO END Aufgabe 2 Funktionalitaet 3 */
+        //aufgabeEins(adj); // Beschreibung siehe Methode
+        //aufgabeZwei(adj); // Beschreibung siehe Methode
 
         // Kuerzesten Weg berechnen
         DPQ dpq = new DPQ(V);
@@ -132,6 +92,40 @@ public class DPQ {
         for (int i = 0; i < dpq.dist.length; i++)
             System.out.println(source + " to " + i + " is "
                     + dpq.dist[i]);
+    }
+
+    /**
+     * TODO Aufgabe 1
+     * Adjazenzliste welche zuvor erstellt wurde einprogrammieren
+     *
+     * @param adj Beispiel Input für den DPQ Graphen
+     *            adj.get(0).add(new Node(1, 9));
+     *            adj.get(0).add(new Node(2, 6));
+     *            adj.get(0).add(new Node(3, 5));
+     *            adj.get(0).add(new Node(4, 3));
+     *            adj.get(2).add(new Node(1, 2));
+     *            adj.get(2).add(new Node(3, 4));
+     */
+    private static void aufgabeEins(@NotNull List<List<Node>> adj) {
+        // TODO ADD CODE HERE
+    }
+
+    /**
+     * TODO Aufgabe 2
+     * Teil 1: Konsolenanwendung entwickeln, mit welcher es möglich ist,
+     * einen Graphen wie in Aufgabe 1 über die Kommandozeile einzutragen.
+     * <p>
+     * Teil 2: Es soll möglich sein, mittels eines Kommandos den Graphen auszugeben
+     * (entweder eine Adjazenzliste oder eine grafische Darstellung)
+     * <p>
+     * Teil 3: Eine zusaetzliche Funktion implementieren, wie das entfernen eines zuvor eingetragenen
+     * Kante
+     * <p>
+     * Teil 4: Sollte noch Zeit uebrig sein, eine weitere Funktion deiner Wahl implementieren.
+     * Fehlerbehandlung bei falschen Input, Parsing, ... beruecksichtigen!
+     */
+    public static void aufgabeZwei(@NotNull List<List<Node>> adj) {
+        // TODO ADD CODE HERE
     }
 }
 
@@ -159,4 +153,7 @@ class Node implements Comparator<Node> {
             return 1;
         return 0;
     }
+
+    // TODO hier koennen Hilfsmethoden hinzugefuegt werden.
 }
+
